405546d94cb5496df67c55da5731f23914a3940b
Aman-Soule <amansoule9@gmail.com>
2025-12-03 11:24:23 +0000

    index

--- public/index.php
+++ public/index.php
@@ -1,5 +1,21 @@
-<?php
-// Redirection vers la page de connexion par défaut
-header("Location: page/connexion.php");
-exit;
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="">
+    <script src=""></script>
+</head>
+<body>
+    <h1>Page de Connexion</h1>
+    <form method="post" action="#">
+        <label>Nom d'utilisateur :</label>
+        <input type="text" name="username"><br>
+        <label>Mot de passe :</label>
+        <input type="password" name="password"><br>
+        <button type="submit">Se connecter</button>
+        
+    </form>
+    <p><a href="inscription.php">Créer un compte</a></p>
+</body>
+</html>
