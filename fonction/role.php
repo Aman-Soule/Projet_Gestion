f0e69d747a0ba2257d1a9615c31a8986b4dd4219
Aman-Soule <amansoule9@gmail.com>
2025-12-03 11:05:38 +0000

    reset

--- fonction/role.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once("connectBD.php");
-    function getRole(){
-        $sql = "SELECT * FROM role";
-        $connect = getBDconnexion();
-        $exec = $connect->query($sql);
-        return $exec->fetchAll();
-    }
-?>
